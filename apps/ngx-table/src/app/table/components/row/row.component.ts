1bda2ba138e8ae8cecd170e0e29762e6a6f3db06
michago <m.gonser@icloud.com>
2020-04-10 14:18:32 +0200

    styling: add hover-behavior on row

--- apps/ngx-table/src/app/table/components/row/row.component.ts
+++ apps/ngx-table/src/app/table/components/row/row.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  HostBinding,
+  HostListener
+} from '@angular/core';
 import { RowDefinition } from '../../models/table-models';
 
 @Component({
@@ -7,7 +13,25 @@ import { RowDefinition } from '../../models/table-models';
   styleUrls: ['./row.component.scss']
 })
 export class RowComponent implements OnInit {
+  // TODO when selected then some highlighting
+  // TODO Highlighting on Hover
   @Input() row: RowDefinition = null;
+
+  @HostBinding('style.border-top') borderTop: string = '1px solid transparent';
+  @HostBinding('style.border-bottom') borderBottom: string = '1px solid #ccc';
+  @HostBinding('style.background-color') backgroundColor: string = 'white';
+
+  @HostListener('mouseenter') mouseOver() {
+    this.borderTop = '1px solid #4a5568';
+    this.borderBottom = '1px solid #4a5568';
+    this.backgroundColor = '#e2e8f0';
+  }
+
+  @HostListener('mouseleave') mouseLeave() {
+    this.borderTop = '1px solid transparent';
+    this.borderBottom = '1px solid #ccc';
+    this.backgroundColor = 'white';
+  }
   constructor() {}
 
   ngOnInit() {}
