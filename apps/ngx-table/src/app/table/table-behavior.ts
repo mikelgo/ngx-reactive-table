8d776a240742723091878b68ea8512178a590405
michago <m.gonser@icloud.com>
2020-04-26 10:50:56 +0200

    interface enhancement

--- apps/ngx-table/src/app/table/table-behavior.ts
+++ apps/ngx-table/src/app/table/table-behavior.ts
@@ -1,6 +1,7 @@
 // import { RowDefinition } from './models/table-models';
 import { Observable } from 'rxjs';
 import { DataRow } from './models/data-row.model';
+import { TitleColumn } from './models/title-column.model';
 
 /**
  * Defines common behavior a table has
@@ -10,4 +11,7 @@ export interface TableBehavior {
   onRowSelect(row: DataRow, rowIndex: number): void;
   getSelectedRows(): Observable<DataRow[]>;
   getSelectedRowsCount(): Observable<number>;
+
+  onColumnShow(column: TitleColumn): void;
+  onColumnHide(column: TitleColumn): void;
 }
