ace89c62fdb1edcc46623c192f9777de22e5644b
michago <m.gonser@icloud.com>
2020-04-29 12:54:45 +0200

    refactoring/datasource: refactor TableDatasource to be more reactive

--- apps/ngx-table/src/app/datasource/datasource.ts
+++ apps/ngx-table/src/app/datasource/datasource.ts
@@ -1,15 +1,27 @@
+import { BehaviorSubject, Observable } from 'rxjs';
+
 export interface Datasource<T> {
   // connect(data: T[]): void;
-
+  /**
+   * Data stream
+   */
+  data$: Observable<T[]>;
   getData(): T[];
   setData(data: T[]): void;
+  /**
+   * Returns a snapshot of the current data
+   */
+  getDataSnapshot(): T[] | null;
 }
 
 export class TableDatasource<T> implements Datasource<T> {
   private _data: T[] = [];
+  private data$$: BehaviorSubject<T[]> = new BehaviorSubject(null);
+  public data$: Observable<T[]> = this.data$$.asObservable();
 
   constructor(data: T[]) {
     this._data = data;
+    this.data$$.next(data);
   }
 
   getData(): T[] {
@@ -18,5 +30,14 @@ export class TableDatasource<T> implements Datasource<T> {
 
   setData(d: T[]): void {
     this._data = d;
+    this.data$$.next(d);
+  }
+
+  getDataSnapshot() {
+    if (this.data$$) {
+      return this.data$$.getValue();
+    } else {
+      return null;
+    }
   }
 }
