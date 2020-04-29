400bad4e58caef56003f6554b04937b3a919d599
michago <m.gonser@icloud.com>
2020-04-29 15:08:56 +0200

    feature/striped-rows: define config interface

--- apps/ngx-table/src/app/table/models/row-config.ts
+++ apps/ngx-table/src/app/table/models/row-config.ts
@@ -1,3 +1,5 @@
+import { StripedStyleConfig } from './striped-style-config';
+
 export interface RowConfig {
   /**
    * Default row style.
@@ -16,4 +18,10 @@ export interface RowConfig {
    * Default is false.
    */
   striped?: boolean;
+  /**
+   * Custom configuration for striped rows.
+   * Styles are only applied if config is provided and
+   * 'striped' is set to true.
+   */
+  stripedStyleConfig?: StripedStyleConfig;
 }
---
+++ apps/ngx-table/src/app/table/models/striped-style-config.ts
@@ -0,0 +1,17 @@
+export interface StripedStyleConfig {
+  /**
+   * Border style of the top border.
+   * Example: '1px solid black'
+   */
+  topBorderStyle: string;
+  /**
+   * Border style of the bottom border.
+   * Example: '1px solid black'
+   */
+  bottomBorderStyle: string;
+  /**
+   * Background color.
+   * Example: 'white'
+   */
+  backgroundColor: string;
+}
