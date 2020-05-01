b4f4c2432a9c38b2f3010056b362fdbb8ccf565d
michago <m.gonser@icloud.com>
2020-05-01 17:17:32 +0200

    body-component: update tests

--- libs/ngx-reactive-table/src/lib/table/components/body/body.component.spec.ts
+++ libs/ngx-reactive-table/src/lib/table/components/body/body.component.spec.ts
@@ -1,28 +1,26 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
 import { BodyComponent } from './body.component';
+import { DEFAULT_TABLE_CONFIG } from '../../config/table-config';
+import { TableConfig } from '../../models/public-api';
 
 describe('BodyComponent', () => {
   let component: BodyComponent;
-  let fixture: ComponentFixture<BodyComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BodyComponent ]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(BodyComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new BodyComponent();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have an initial maxBodyHeight provided by the default table config, when no config is passed as input', () => {
+    expect(component.maxBodyHeight).toEqual(DEFAULT_TABLE_CONFIG.maxBodyHeight);
+  });
+  it('should adjust the maxBodyHeight when a config is passed as input', () => {
+    const config: TableConfig = {
+      maxBodyHeight: '400px'
+    };
+    component.config = config;
+    expect(component.maxBodyHeight).toEqual(config.maxBodyHeight);
   });
 });
