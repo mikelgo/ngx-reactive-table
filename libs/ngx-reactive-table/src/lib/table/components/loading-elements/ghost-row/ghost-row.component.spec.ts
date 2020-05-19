6d1b92f9de9f224a6811e0b877018379bb238cdb
michago <m.gonser@icloud.com>
2020-05-19 13:23:28 +0200

    ghost-row-component: add unit tests

--- libs/ngx-reactive-table/src/lib/table/components/loading-elements/ghost-row/ghost-row.component.spec.ts
+++ libs/ngx-reactive-table/src/lib/table/components/loading-elements/ghost-row/ghost-row.component.spec.ts
@@ -4,25 +4,44 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { GhostRowComponent } from './ghost-row.component';
+import { DEFAULT_ROW_CONFIG } from '../../../config/table-config';
+import { RowConfig } from '../../../models/row-config';
 
 describe('GhostRowComponent', () => {
   let component: GhostRowComponent;
-  let fixture: ComponentFixture<GhostRowComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ GhostRowComponent ]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GhostRowComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new GhostRowComponent();
+  });
+
+  afterEach(() => {
+    component = null;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set a valid row config via input', () => {
+    expect(component.config).toEqual(DEFAULT_ROW_CONFIG);
+
+    const config: RowConfig = {
+      height: '40px'
+    };
+
+    component.config = config;
+    expect(component.config).toEqual(config);
+  });
+  it('should NOT set the row config when the config is not valid', () => {
+    component.config = null;
+    expect(component.config).not.toEqual(null);
+  });
+
+  it('should have an inital height which is defined in a config', () => {
+    expect(component.height).toEqual(DEFAULT_ROW_CONFIG.height);
+  });
+
+  it('get config() should return the row', () => {
+    expect(component.config).toEqual(component.config);
   });
 });
