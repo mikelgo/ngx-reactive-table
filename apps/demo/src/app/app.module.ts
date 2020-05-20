9843a7ea7a1e7a11f622acf26ae9aa1bd8d87af9
michago <m.gonser@icloud.com>
2020-05-20 15:13:58 +0200

    demo: initial demo setup

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -9,7 +10,8 @@ import { NgxReactiveTableModule } from '@ngx-table/ngx-reactive-table';
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
-    NgxReactiveTableModule
+    NgxReactiveTableModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
