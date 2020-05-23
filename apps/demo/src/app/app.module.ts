1a3b030e05e5bbfa1dad78c79399a7ecad0fd56d
michago <m.gonser@icloud.com>
2020-05-23 13:54:22 +0200

    demo: configuration of ngx-markdown and showing first example

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule, SecurityContext } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
@@ -8,7 +8,26 @@ import { NgxReactiveTableModule } from '@ngx-table/ngx-reactive-table';
 
 import { Example1Component } from './examples/example-1/example-1.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { MarkdownModule } from 'ngx-markdown';
+import { MarkdownModule, MarkedOptions, MarkedRenderer  } from 'ngx-markdown';
+
+// function that returns `MarkedOptions` with renderer override
+export function markedOptionsFactory(): MarkedOptions {
+  const renderer = new MarkedRenderer();
+
+  renderer.blockquote = (text: string) => {
+    return '<blockquote class="blockquote"><p>' + text + '</p></blockquote>';
+  };
+
+  return {
+    renderer: renderer,
+    gfm: true,
+    breaks: false,
+    pedantic: false,
+    smartLists: true,
+    smartypants: false,
+  };
+}
+
 @NgModule({
   declarations: [AppComponent, Example1Component],
   imports: [
@@ -17,7 +36,14 @@ import { MarkdownModule } from 'ngx-markdown';
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     NgxReactiveTableModule,
     BrowserAnimationsModule,
-    MarkdownModule.forRoot({ loader: HttpClient })
+    MarkdownModule.forRoot({
+      loader: HttpClient, // optional, only if you use [src] attribute
+      sanitize: SecurityContext.NONE,
+      markedOptions: {
+        provide: MarkedOptions,
+        useFactory: markedOptionsFactory,
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
