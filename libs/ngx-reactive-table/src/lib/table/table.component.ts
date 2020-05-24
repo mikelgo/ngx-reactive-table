d9a2c4e553562efad03f1b5bcf35313f7cf5b062
michago <m.gonser@icloud.com>
2020-05-24 09:56:12 +0200

    feat(lib/table): use footer by applying withFooter directive

--- libs/ngx-reactive-table/src/lib/table/table.component.ts
+++ libs/ngx-reactive-table/src/lib/table/table.component.ts
@@ -6,7 +6,9 @@ import {
   OnDestroy,
   Output,
   EventEmitter,
-  ChangeDetectionStrategy
+  ChangeDetectionStrategy,
+  Optional,
+  Host
 } from '@angular/core';
 import { TableConfig } from './models/table-config';
 import { DEFAULT_TABLE_CONFIG } from './config/table-config';
@@ -21,6 +23,7 @@ import { takeUntil, tap, map } from 'rxjs/operators';
 import { HiddenColumns } from './models/hidden-column.model';
 import { calcAdjustedWidths } from '../shared/util/calculate-normalized-widths';
 import { parseUnit } from '../shared/util/parse-unit';
+import { WithFooterDirective } from './directives/with-footer.directive';
 
 @Component({
   selector: 'ngx-table',
@@ -81,7 +84,6 @@ export class TableComponent<T> implements OnInit, TableBehavior, OnDestroy {
 
   public hiddenColumns$: Observable<TitleColumn[]>;
   public hiddenColumnsCount$: Observable<number>;
-
   public hiddenColumnsInfo$: Observable<HiddenColumns>;
 
   public columnWidth: string;
@@ -89,7 +91,7 @@ export class TableComponent<T> implements OnInit, TableBehavior, OnDestroy {
   private displayColumnWidths$$ = new Subject();
   public displayColumnWidths$: Observable<string>;
 
-  constructor(public stateService: TableStateService<T>) {}
+  constructor(public stateService: TableStateService<T>, @Optional() @Host() public withFooter: WithFooterDirective) {}
 
   ngOnInit() {
     this.displayColumnWidths$ = this.stateService.renderHeaders$.pipe(
@@ -126,6 +128,7 @@ export class TableComponent<T> implements OnInit, TableBehavior, OnDestroy {
     //     this.displayColumnWidths$$.next(this.getColumnWidths(header))
     //   );
   }
+
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
